344576d298747afc9097539352bd2c4818f2daf4
Muneebur Rahman <muneeburrahman0213@gmail.com>
2025-12-25 15:39:58 +0530

    Create IfElse.java

---
+++ Basics/IfElse.java
@@ -0,0 +1,18 @@
+/*
+Program Name : IfElse
+Description  : Demonstrates the use of if-else conditional statements in Java.
+Author       : Muneebur Rahman
+Created On   : 25-Dec-2025
+ */
+
+public class IfElse {
+
+    public static void main(String[] args) {
+        int number = 10;
+        if (number > 0) {
+            System.err.println("Positive Number");
+        } else {
+            System.err.println("Non-positive Number");
+        }
+    }
+}

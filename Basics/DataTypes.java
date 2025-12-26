0673f17fa4decf8fd5d12d6bfc56505210b46216
Muneebur Rahman <muneeburrahman0213@gmail.com>
2025-12-26 08:38:29 +0530

    Added DataTypes

---
+++ Basics/DataTypes.java
@@ -0,0 +1,22 @@
+/*
+Program Name : HelloWorld
+Description  : This program demonstrates the use of basic primitive data types in java.
+Author       : Muneebur Rahman
+*/
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+        int age = 20;
+        float price = 95.9f;
+        double pi = 3.14159;
+        char grade = 'A';
+        boolean isPassed = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Price: " + price);
+        System.out.println("PI: " + pi);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + isPassed);
+    }
+}

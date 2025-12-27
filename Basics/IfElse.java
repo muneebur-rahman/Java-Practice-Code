74df647610ef810f4cfbe9f17072e9af8837f14b
Muneebur Rahman <muneeburrahman0213@gmail.com>
2025-12-27 22:09:29 +0530

    Update IfElse.java

--- Basics/IfElse.java
+++ Basics/IfElse.java
@@ -9,9 +9,9 @@ public class IfElse {
     public static void main(String[] args) {
         int number = 10;
         if (number > 0) {
-            System.out.println("Positive Number!!");
+            System.out.println("Positive Number!!!");
         } else {
-            System.out.println("Non-positive Number!!");
+            System.out.println("Non-positive Number!!!");
         } // else
     }// main
 }// class

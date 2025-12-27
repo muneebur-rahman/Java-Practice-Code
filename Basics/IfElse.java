e115b7f35e28bcfd10934a07f8bcf37b934840d4
Muneebur Rahman <muneeburrahman0213@gmail.com>
2025-12-27 21:47:35 +0530

    Add IfElse.java

--- Basics/IfElse.java
+++ Basics/IfElse.java
@@ -2,7 +2,6 @@
 Program Name : IfElse
 Description  : Demonstrates the use of if-else conditional statements in Java.
 Author       : Muneebur Rahman
-Created On   : 25-Dec-2025
  */
 
 public class IfElse {
@@ -10,9 +9,9 @@ public class IfElse {
     public static void main(String[] args) {
         int number = 10;
         if (number > 0) {
-            System.err.println("Positive Number");
+            System.out.println("Positive Number!");
         } else {
-            System.err.println("Non-positive Number");
-        }//else
-    }//main
-}//class
+            System.out.println("Non-positive Number!");
+        } // else
+    }// main
+}// class

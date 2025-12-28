1db0dcf09f750b29910a1008b6e39238ce2a8382
Muneebur Rahman <muneeburrahman0213@gmail.com>
2025-12-28 23:27:42 +0530

    Added while Loop

---
+++ Basics/While.java
@@ -0,0 +1,15 @@
+/*
+Program Name : While Loop to print numbers.
+Description  : Demonstrate the use of While Loop in java.
+Author       : Muneebur Rahman
+ */
+public class While {
+    public static void main(String[] args) {
+        int i=1;
+        System.err.println("Number from 1 to 10 : ");
+        while (i<=10) {
+            System.err.println(i);
+            i++;
+        }//while
+    } //main
+}//class

4d0aec2efab878bab0b63e219ffc6c7d65f88d3c
Muneebur Rahman <muneeburrahman0213@gmail.com>
2025-12-29 23:56:09 +0530

    Added do-while

---
+++ Basics/DoWhile.java
@@ -0,0 +1,16 @@
+/*
+Program Name : While Loop to print numbers.
+Description  : Demonstrate the use of While Loop in java.
+Author       : Muneebur Rahman
+ */
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println("Number from 1 to 10 : ");
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 10);
+    }
+}

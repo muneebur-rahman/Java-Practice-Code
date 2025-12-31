3e186bf2a9b50993ec19622bbea05e54f62de9b6
Muneebur Rahman <muneeburrahman0213@gmail.com>
2026-01-01 00:21:09 +0530

    Added if example

---
+++ Basics/If.java
@@ -0,0 +1,14 @@
+/*
+Program Name : If-Else
+Description  : Demonstrate the use of if conditional statements in Java.
+Author       : Muneebur Rahman
+ */
+public class If {
+    public static void main(String[] args) {
+        int age = 20;
+        if (age > 18) {
+            System.out.println("Age iss greater then 18!!");
+        } // if
+        System.out.println("Outside of if block!!");
+    }// main
+}// class

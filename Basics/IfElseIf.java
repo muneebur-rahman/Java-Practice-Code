e2e590dd20bc9d72ad62b03087b373adcb7bd27b
Muneebur Rahman <muneeburrahman0213@gmail.com>
2026-01-02 23:05:36 +0530

    Update IfElseIf

--- Basics/IfElseIf.java
+++ Basics/IfElseIf.java
@@ -1,29 +1,29 @@
 /*
-Program Name : If-Else
+Program Name : If-Else If-Else
 Description  : Demonstrate the use of if-else if-else conditional statements in Java.
 Author       : Muneebur Rahman
  */
 
 public class IfElseIf {
     public static void main(String[] args) {
-        int GradePoints=9;
+        int GradePoints = 9;
 
-        if(GradePoints==10){
+        if (GradePoints == 10) {
             System.out.println("Letter Grade : O");
-        }else if(GradePoints==9){
+        } else if (GradePoints == 9) {
             System.out.println("Letter Grade : A+");
-        }else if(GradePoints==8){
+        } else if (GradePoints == 8) {
             System.out.println("Letter Grade : A");
-        }else if(GradePoints==7){
+        } else if (GradePoints == 7) {
             System.out.println("Letter Grade : B+");
-        }else if(GradePoints==6){
+        } else if (GradePoints == 6) {
             System.out.println("Letter Grade : B");
-        }else if(GradePoints==5){
+        } else if (GradePoints == 5) {
             System.out.println("Letter Grade : C");
-        }else if(GradePoints==4){
+        } else if (GradePoints == 4) {
             System.out.println("Letter Grade : P");
-        }else{
+        } else {
             System.out.println("Letter Grade : F");
-        }//else
-    }//main
-}//class
+        } // else
+    }// main
+}// class

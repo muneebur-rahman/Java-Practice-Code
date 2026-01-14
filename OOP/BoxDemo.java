0f3e7fca7bd220c1c595609f986adf2448064cd4
Muneebur Rahman <muneeburrahman0213@gmail.com>
2026-01-14 22:44:29 +0530

    Added Box Demo

---
+++ OOP/BoxDemo.java
@@ -0,0 +1,25 @@
+/*
+Program Name : Calculalte Volume (Objects and classes Example).
+Description  : Simple Program demonstrating objects and classes in OOP in java .
+Author       : Muneebur Rahman
+ */
+
+package OOP;
+
+class Box {
+    double width;
+    double height;
+    double depth;
+}
+
+public class BoxDemo {
+    public static void main(String[] args) {
+        Box myBox = new Box();
+        double vol;
+        myBox.width = 10;
+        myBox.height = 20;
+        myBox.depth = 15;
+        vol = myBox.width * myBox.height * myBox.depth;
+        System.out.println("Volume = " + vol);
+    }
+}

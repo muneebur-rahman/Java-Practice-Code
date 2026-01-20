84f46671923f88cf1e87cab2995509ffa74d0c08
Muneebur Rahman <muneeburrahman0213@gmail.com>
2026-01-20 23:42:52 +0530

    Added Main

---
+++ OOP/Main.java
@@ -0,0 +1,20 @@
+package OOP;
+
+class Demo {
+    int x = 10;
+
+    int display() {
+        System.out.println("x = " + x);
+        return 0;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Demo D1 = new Demo();
+
+        System.out.println(D1); //print garbage value
+
+        System.out.println(D1.display());
+    }
+}

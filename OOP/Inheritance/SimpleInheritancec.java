84896c3e2d8d0c2372b2ca9ba1d0fd50bcbfe614
Muneebur Rahman <muneeburrahman0213@gmail.com>
2026-01-27 22:30:57 +0530

    Update SimpleInheritancec.java

--- OOP/Inheritance/SimpleInheritancec.java
+++ OOP/Inheritance/SimpleInheritancec.java
@@ -2,7 +2,7 @@ package OOP.Inheritance;
 
 class A{
     int i,j;
-    void showj(){
+    void showij(){
         System.out.println("i and j:"+i+" "+j);
     }
 }
@@ -17,6 +17,23 @@ class B extends A{
 }
 public class SimpleInheritancec {
     public static void main(String[] args) {
+        A a=new A();
+        B b=new B();
+
+        a.i=10;
+        a.j=20;
+        System.out.println("Contents of Super object (A)");
+        a.showij();
+        System.out.println();
         
+        b.i=7;
+        b.j=8;
+        b.k=9;
+        System.out.println("Contents of sub object (B)");
+        b.showij();
+        b.showk();
+        System.out.println("Sum of i j and k is: ");
+        b.sum();
+
     }
 }

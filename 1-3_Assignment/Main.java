a06398b2ee45fa835df50405a5b82b3fc85c9d14
Muneebur Rahman <muneeburrahman0213@gmail.com>
2026-03-10 23:20:15 +0530

    Update Main.java

--- 1-3_Assignment/Main.java
+++ 1-3_Assignment/Main.java
@@ -16,6 +16,9 @@ public class Main {
     public static void main(String[] args) {
         Person p1 = new Person("Muneeb", 19);
         Person p2 = new Person("Noman", 21);
-        
+        System.out.println(p1.name);
+        System.out.println(p1.age);
+        System.out.println(p2.name);
+        System.out.println(p2.age);
     }
 }

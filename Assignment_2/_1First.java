2226bd4f500480917676ecdc16a49366fc2e6e13
Muneebur Rahman <muneeburrahman0213@gmail.com>
2026-03-28 22:04:02 +0530

    Create _1First.java

---
+++ Assignment_2/_1First.java
@@ -0,0 +1,11 @@
+// package Assignment_2;
+
+// 1. Create a class called UsernameValidator.
+//  Accept a username from the user.
+//  Remove leading and trailing spaces.
+//  Convert the username to lowercase.
+//  Display the cleaned username.
+
+public class _1First {
+    
+}

94f93a72c194271d9019695718e8a735e8f3cbb0
Muneebur Rahman <muneeburrahman0213@gmail.com>
2026-03-29 16:46:05 +0530

    Update _1First.java

--- Assignment_2/_1First.java
+++ Assignment_2/_1First.java
@@ -6,6 +6,25 @@
 //  Convert the username to lowercase.
 //  Display the cleaned username.
 
+import java.util.Scanner;
+
 public class _1First {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Accept username
+        System.out.print("Enter username: ");
+        String username = sc.nextLine();
+
+        // Remove leading and trailing spaces
+        username = username.trim();
+
+        // Convert to lowercase
+        username = username.toLowerCase();
+
+        // Display cleaned username
+        System.out.println("Cleaned Username: " + username);
+
+        sc.close();
+    }
 }

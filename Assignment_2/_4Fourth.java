4757cec98df93391c22fac61be5b51faf76f884c
Muneebur Rahman <muneeburrahman0213@gmail.com>
2026-04-01 23:56:16 +0530

    Update _4Fourth.java

--- Assignment_2/_4Fourth.java
+++ Assignment_2/_4Fourth.java
@@ -1,5 +1,8 @@
-
+// 4. Create a class MessageFormatter.
+//  Accept first name and last name.
+//  Concatenate both names with space.
+//  Display full name in proper format.
 
 public class _4Fourth {
-    
+
 }

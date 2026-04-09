7a5fcafa2b4539d3f8dc4e450622c9b867db6575
Muneebur Rahman <muneeburrahman0213@gmail.com>
2026-04-09 19:54:34 +0530

    Create _11Eleventh.java

---
+++ Assignment_2/_11Eleventh.java
@@ -0,0 +1,37 @@
+// 11. ATM Withdrawal System
+// Create a class ATMWithdrawal. Accept account balance and
+
+// withdrawal amount from the user. If withdrawal amount is
+// greater than balance, throw an exception manually using throw.
+// Otherwise deduct amount and display updated balance.
+// Output: If sufficient balance: &#39;Withdrawal successful.
+// Remaining balance: &lt;balance&gt;&#39; Else: &#39;Insufficient Balance&#39;
+
+
+// import java.util.Scanner;
+
+public class _11Eleventh{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter account balance: ");
+        double balance = sc.nextDouble();
+
+        System.out.print("Enter withdrawal amount: ");
+        double withdraw = sc.nextDouble();
+
+        try {
+            if (withdraw > balance) {
+                throw new Exception("Insufficient Balance");
+            } else {
+                balance = balance - withdraw;
+                System.out.println("Withdrawal successful.");
+                System.out.println("Remaining balance: " + balance);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        sc.close();
+    }
+}
